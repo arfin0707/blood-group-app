784f2d46f4d593df6617552529fd849610f7cd05
arfin0707 <150445577+arfin0707@users.noreply.github.com>
2025-07-07 23:25:14 +0600

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    #st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     transform = ConvNeXt_Tiny_Weights.DEFAULT.transforms()
     input_tensor = transform(image).unsqueeze(0)
