67b1941c4abf7058569b21dc4217f200c00b71e8
arfin0707 <150445577+arfin0707@users.noreply.github.com>
2025-07-11 00:14:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -52,15 +52,15 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_container_width=True)
 
-    # transform = ConvNeXt_Tiny_Weights.DEFAULT.transforms()
-    transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(10),
-    transforms.ColorJitter(),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
+    transform = ConvNeXt_Tiny_Weights.DEFAULT.transforms()
+    # transform = transforms.Compose([
+    # transforms.Resize((224, 224)),
+    # transforms.RandomHorizontalFlip(),
+    # transforms.RandomRotation(10),
+    # transforms.ColorJitter(),
+    # transforms.ToTensor(),
+    # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    # ])
     
     input_tensor = transform(image).unsqueeze(0)
 
