7d2b9605cc5855634e0b04cb7e760a6bb5e0bc76
Israth Arfin <arfinhabib07@gmail.com>
2025-09-29 21:28:48 +0600

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,9 @@ uploaded_file = st.file_uploader("Upload a fingerprint image", type=["jpg", "jpe
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    # st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(image, caption="Uploaded Image", width=250)
+
 
     # Apply ConvNeXt-Base default transforms
     transform = ConvNeXt_Base_Weights.DEFAULT.transforms()
